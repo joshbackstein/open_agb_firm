924dd5a3e3cf229fea3543f4a501f81d765ef392
Josh Backstein <josh.backstein@gmail.com>
2023-04-11 00:39:50 -0600

    Add default autoboot to rom.gba on root of SD card

--- source/arm11/open_agb_firm.c
+++ source/arm11/open_agb_firm.c
@@ -59,6 +59,7 @@
                         "[advanced]\n"            \
                         "saveOverride=false\n"    \
                         "defaultSave=14"
+#define DEFAULT_AUTOBOOT "sdmc:/rom.gba"
 
 typedef struct
 {
@@ -745,6 +746,15 @@ Result oafInitAndRun(void)
 	{
 		do
 		{
+			// Create autoboot.txt for default rom if it doesn't exist
+			if((res = fsLoadPathFromFile("autoboot.txt", filePath)) == RES_FR_NO_FILE)
+			{
+				const char *const defaultAutoboot = DEFAULT_AUTOBOOT;
+				if((res = fsQuickWrite("autoboot.txt", defaultAutoboot, strlen(defaultAutoboot))) != RES_OK) break;
+			}
+			else if(res != RES_OK) break;
+			memset(filePath, 0, 512);
+
 			// Try to load the ROM path from autoboot.txt.
 			// If this file doesn't exist show the file browser.
 			if((res = fsLoadPathFromFile("autoboot.txt", filePath)) == RES_FR_NO_FILE)
